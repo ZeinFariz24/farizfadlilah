f112a9b0caeda8ef5a59090832e70fae39e660bc
Zein24 <70275033+ZeinFariz24@users.noreply.github.com>
2024-06-24 21:18:17 +0700

    Upload js

---
+++ js/script.js
@@ -0,0 +1,127 @@
+/*
+* Copyright 2024 Fariz Fadlilah
+*/
+
+// dark mode
+(() => {
+    'use strict'
+  
+    const getStoredTheme = () => localStorage.getItem('theme')
+    const setStoredTheme = theme => localStorage.setItem('theme', theme)
+  
+    const getPreferredTheme = () => {
+      const storedTheme = getStoredTheme()
+      if (storedTheme) {
+        return storedTheme
+      }
+  
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+  
+    const setTheme = theme => {
+      if (theme === 'auto') {
+        document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+      } else {
+        document.documentElement.setAttribute('data-bs-theme', theme)
+      }
+    }
+  
+    setTheme(getPreferredTheme())
+  
+    const showActiveTheme = (theme, focus = false) => {
+      const themeSwitcher = document.querySelector('#bd-theme')
+  
+      if (!themeSwitcher) {
+        return
+      }
+  
+      const themeSwitcherText = document.querySelector('#bd-theme-text')
+      const activeThemeIcon = document.querySelector('.theme-icon-active')
+      const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+      const iconOfActiveBtn = btnToActive.querySelector('i').dataset.themeIcon
+  
+      document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+        element.classList.remove('active')
+        element.setAttribute('aria-pressed', 'false')
+      })
+  
+      btnToActive.classList.add('active')
+      activeThemeIcon.classList.remove(activeThemeIcon.theme.dataset.themeIconActive)
+      activeThemeIcon.classList.add(iconOfActiveBtn)
+      activeThemeIcon.dataset.iconOfActive = iconOfActiveBtn
+
+      const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
+      themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
+  
+      if (focus) {
+        themeSwitcher.focus()
+      }
+    }
+  
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      const storedTheme = getStoredTheme()
+      if (storedTheme !== 'light' && storedTheme !== 'dark') {
+        setTheme(getPreferredTheme())
+      }
+    })
+  
+    window.addEventListener('DOMContentLoaded', () => {
+      showActiveTheme(getPreferredTheme())
+  
+      document.querySelectorAll('[data-bs-theme-value]')
+        .forEach(toggle => {
+          toggle.addEventListener('click', () => {
+            const theme = toggle.getAttribute('data-bs-theme-value')
+            setStoredTheme(theme)
+            setTheme(theme)
+            showActiveTheme(theme, true)
+          })
+        })
+    })
+  })()
+// /dark mode
+
+// Landing
+// index
+const navbarUI = document.querySelector('#navbarLand');
+const navbarContainer = document.querySelector('#navbarContainer');
+window.addEventListener('scroll', () => {
+    if(this.scrollY > 0){
+        navbarUI.classList.remove('bg-tansparant')
+        navbarUI.classList.add('bg-body-tertiary')
+        navbarUI.classList.add('shadow')
+        navbarContainer.classList.remove('shadow-sm')
+    } else {
+        navbarUI.classList.add('bg-tansparant')
+        navbarUI.classList.remove('bg-body-tertiary')
+        navbarUI.classList.remove('shadow')
+        navbarContainer.classList.add('shadow-sm')
+    }
+});
+
+// index
+// Landing
+
+// ___________________________
+// ___________________________
+// ___________________________
+
+// dashboard
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+// /dashboard
